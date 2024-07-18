b834b3effafed794adc6d81ebb34458a7ae69a69
Luigi <luigicumaru@gmail.com>
2024-07-17 22:49:57 -0300

    Resolução de Bugs

--- Barbearia/obj/Debug/net8.0/Barbearia.AssemblyInfo.cs
+++ Barbearia/obj/Debug/net8.0/Barbearia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barbearia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb41a71a893925282c5c211b306c27850e68fa70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad761124d8ca32916234eb762654a207dcd576e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barbearia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barbearia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
